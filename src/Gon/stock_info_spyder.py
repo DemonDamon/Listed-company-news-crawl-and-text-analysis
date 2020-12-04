ff9fa024fed0700e83dc4a431e2b0c6c0c1b030d
DemonDamon <bingzhenli@hotmail.com>
2020-12-04 14:06:40 +0800

    Project updated by Damon

--- src/Gon/stock_info_spyder.py
+++ src/Gon/stock_info_spyder.py
@@ -11,15 +11,22 @@ ts.set_token(config.TUSHARE_TOKEN)
 
 import akshare as ak
 
-class TushareSpyder(Spyder):
+
+class StockInfoSpyder(Spyder):
 
     def __init__(self):
-        super(TushareSpyder, self).__init__()
+        super(StockInfoSpyder, self).__init__()
         self.db = self.db_obj.create_db(config.TUSHARE_DATABASE_NAME)
         self.col_basic_info = self.db_obj.create_col(self.db, config.COLLECTION_NAME_STOCK_BASIC_INFO)
+
+    def get_stock_code_info(self):
         stock_info_a_code_name_df = ak.stock_info_a_code_name()
-        stock_info_a_code_name_df.iloc[0].to_dict()
+        for _id in range(stock_info_a_code_name_df.shape[0]):
+            _dict = stock_info_a_code_name_df.iloc[_id].to_dict()
+            self.col.insert_one(_dict)
 
-    def get_stock_code
+    def get_historical_news(self):
+        pass
 
-    def get_historical_news
+
+if __name__ == "__main__":
