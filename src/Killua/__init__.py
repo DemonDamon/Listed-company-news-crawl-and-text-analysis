743a577d7d8006d88a98a943b2a369eab4ec1f65
DemonDamon <bingzhenli@hotmail.com>
2020-12-09 10:17:09 +0800

    Project updated by Damon

---
+++ src/Killua/__init__.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+
+
+# add `./src` dir to system path
+src_dir = os.path.abspath(os.path.join(os.getcwd(), "../"))
+
+add_path(src_dir)
