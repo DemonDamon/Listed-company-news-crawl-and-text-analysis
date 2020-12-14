16c6b228a90a54c1c08c19ca863c1f9de9ab6fb5
DemonDamon <bingzhenli@hotmail.com>
2020-12-14 13:15:43 +0800

    Project updated by Damon

--- src/Hisoka/classifier.py
+++ src/Hisoka/classifier.py
@@ -31,7 +31,23 @@ class Classifier(object):
             if svm_save_path is not None:
                 joblib.dump(svm_save_path)
             # 输出最优的模型参数
-            logging.info("the best params -> {}".format(clf.best_params_))
+            logging.info("the best params: {}".format(clf.best_params_))
+            train_pred = clf.predict(train_x)
+            test_pred = clf.predict(test_x)  # 在测试集上测试最优的模型的泛化能力
+            logging.info(classification_report(test_y, test_pred))
+
+            precise_train = 0
+            for k in range(len(train_pred)):
+                if train_pred[k] == train_y[k]:
+                    precise_train += 1
+            precise_test = 0
+            for k in range(len(test_pred)):
+                if test_pred[k] == test_y[k]:
+                    precise_test += 1
+            logging.info('train_pred: {}  test_pred: {}'
+                         .format(str(round(precise_train / len(train_y), 4)),
+                                 str(round(precise_test / len(test_pred), 4))))
+            self._precise = precise_test / len(test_pred)
 
     def rd_forest(self):
         pass
