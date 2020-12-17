a670b6776156a18f9a6a778f10040c28fc60b013
DemonDamon <bingzhenli@hotmail.com>
2020-12-17 16:59:09 +0800

    Project updated by Damon

--- src/Leorio/topicmodelling.py
+++ src/Leorio/topicmodelling.py
@@ -21,7 +21,7 @@ class TopicModelling(object):
                                          user_dict=config.USER_DEFINED_DICT_PATH,
                                          chn_stop_words_dir=config.CHN_STOP_WORDS_PATH)
 
-    def create_dictionary(self, raw_documents_list, savepath=None):
+    def create_dictionary(self, raw_documents_list, save_path=None):
         """
         将文中每个词汇关联唯一的ID，因此需要定义词汇表
         :param: raw_documents_list, 原始语料列表，每个元素即文本，如["洗尽铅华...", "风雨赶路人...", ...]
@@ -44,12 +44,37 @@ class TopicModelling(object):
         _dict.filter_tokens(once_ids)
         # 消除id序列在删除词后产生的不连续的缺口
         _dict.compactify()
-        if savepath:
-            _dict.save(savepath)
+        if save_path:
+            _dict.save(save_path)
         return _dict, documents_token_list
 
-    def create_bag_of_word_representation(self, raw_documents_list, dict_save_path=None, bow_vector_save_path=None):
-        corpora_dictionary, documents_token_list = self.create_dictionary(raw_documents_list, savepath=dict_save_path)
+    def renew_dictionary(self, old_dict_path, new_raw_documents_list, new_dict_path=None):
+        documents_token_list = []
+        for doc in new_raw_documents_list:
+            documents_token_list.append(self.tokenization.cut_words(doc))
+        _dict = corpora.Dictionary.load(old_dict_path)
+        _dict.add_documents(documents_token_list)
+        if new_dict_path:
+            old_dict_path = new_dict_path
+        _dict.save(old_dict_path)
+        logging.info("updated dictionary by another raw documents, and serialized in {} ... ".format(old_dict_path))
+        return _dict, documents_token_list
+
+    def create_bag_of_word_representation(self,
+                                          raw_documents_list,
+                                          old_dict_path=None,
+                                          new_dict_path=None,
+                                          bow_vector_save_path=None):
+        if old_dict_path:
+            # 如果存在旧的语料词典，就在原先词典的基础上更新，增加未有的词
+            corpora_dictionary, documents_token_list = self.renew_dictionary(old_dict_path,
+                                                                             raw_documents_list,
+                                                                             new_dict_path=new_dict_path)
+        else:
+            # 否则重新创建词典
+            corpora_dictionary, documents_token_list = self.create_dictionary(raw_documents_list,
+                                                                              save_path=new_dict_path)
+        # 根据新词典对文档(或语料)生成对应的词袋向量
         bow_vector = [corpora_dictionary.doc2bow(doc_token) for doc_token in documents_token_list]
         if bow_vector_save_path:
             corpora.MmCorpus.serialize(bow_vector_save_path, bow_vector)
@@ -82,33 +107,51 @@ class TopicModelling(object):
                 model.save(model_save_path)
         return model_vector
 
-    def add_documents_to_serialized_model(self,
-                                          old_model_path,
-                                          another_raw_documents_list,
-                                          latest_model_path=None,
-                                          model_type="lsi"):
+    def add_unseen_documents_to_serialized_model(self,
+                                                 old_model_path,
+                                                 another_raw_documents_list,
+                                                 latest_model_path=None,
+                                                 model_type="lsi",
+                                                 old_dict_path=None,
+                                                 new_dict_path=None,
+                                                 corpus_save_path=None):
+        loaded_model = None
+        model_vector = None
         # 加载已有的模型，Gensim提供在线学习的模式，不断基于新的documents训练新的模型
         if not os.path.exists(old_model_path):
             raise Exception("the file path {} does not exist ... ".format(old_model_path))
+        # 根据新的文档或者语料更新原有的词典(没有则构造新词典)
+        _, corpora_dictionary, bow_vec = self.create_bag_of_word_representation(another_raw_documents_list,
+                                                                                old_dict_path=old_dict_path,
+                                                                                new_dict_path=new_dict_path,
+                                                                                bow_vector_save_path=corpus_save_path)
+        # 加载历史模型
+        assert model_type in ["lsi", "lda"]
         if model_type == "lsi":
             loaded_model = models.LsiModel.load(old_model_path)
+            another_tfidf_model_vector = self.transform_vectorized_corpus(corpora_dictionary,
+                                                                          bow_vec,
+                                                                          model_type="tfidf")
+            loaded_model.add_documents(another_tfidf_model_vector)
+            model_vector = loaded_model[another_tfidf_model_vector]
         elif model_type == "lda":
             loaded_model = models.LdaModel.load(old_model_path)
-
-        # loaded_model.add_documents(another_tfidf_corpus)
+            loaded_model.update(bow_vec)  # 注意lda和lsi的模型在线更新函数不一样
+            model_vector = loaded_model[bow_vec]
 
         if latest_model_path:
             old_model_path = latest_model_path
+        assert loaded_model and model_vector is not None
         loaded_model.save(old_model_path)
+        return model_vector
 
-
-    def load_transform_model(self, model_path):
-        if ".tfidf" in model_path:
-            return models.TfidfModel.load(model_path)
-        elif ".lsi" in model_path:
-            return models.LsiModel.load(model_path)
-        elif ".lda" in model_path:
-            return models.LdaModel.load(model_path)
+    # def load_transform_model(self, model_path):
+    #     if ".tfidf" in model_path:
+    #         return models.TfidfModel.load(model_path)
+    #     elif ".lsi" in model_path:
+    #         return models.LsiModel.load(model_path)
+    #     elif ".lda" in model_path:
+    #         return models.LdaModel.load(model_path)
 
 
 if __name__ == "__main__":
@@ -121,9 +164,9 @@ if __name__ == "__main__":
     raw_documents_list = []
     Y = []
     for row in database.get_collection("stocknews", "sz000001").find():
-        if "30DaysLabel" in row.keys():
+        if "60DaysLabel" in row.keys():
             raw_documents_list.append(row["Article"])
-            Y.append(row["30DaysLabel"])
+            Y.append(row["60DaysLabel"])
     le = preprocessing.LabelEncoder()
     Y = le.fit_transform(Y)
 
@@ -136,6 +179,9 @@ if __name__ == "__main__":
     classifier = Classifier()
     classifier.svm(train_x, train_y, test_x, test_y)
 
-    # lsi Tue, 15 Dec 2020 14:54:08 classifier.py[line:54] INFO train_pred: 0.9829  test_pred: 0.703 (只是去掉停用词、tab符以及空格符)
-    # lsi Tue, 15 Dec 2020 17:00:58 classifier.py[line:54] INFO train_pred: 0.9852  test_pred: 0.7492(去掉不含中文的词以及只有一个字符的词)
-    # lda Tue, 15 Dec 2020 17:29:56 classifier.py[line:54] INFO train_pred: 0.9498  test_pred: 0.7426(去掉不含中文的词以及只有一个字符的词)
+    # lsi Tue, 15 Dec 2020 14:54:08 classifier.py[line:54] INFO train_pred: 0.9829  test_pred: 0.703 (只是去掉停用词、tab符以及空格符) 30DaysLabel
+    # lsi Tue, 15 Dec 2020 17:00:58 classifier.py[line:54] INFO train_pred: 0.9852  test_pred: 0.7492(去掉不含中文的词以及只有一个字符的词) 30DaysLabel
+    # lda Tue, 15 Dec 2020 17:29:56 classifier.py[line:54] INFO train_pred: 0.9498  test_pred: 0.7426(去掉不含中文的词以及只有一个字符的词) 30DaysLabel
+    # lsi Wed, 16 Dec 2020 15:57:28 classifier.py[line:54] INFO train_pred: 0.9872  test_pred: 0.7478(修改create_dictionary后) 30DaysLabel
+    # lsi Wed, 16 Dec 2020 17:14:57 classifier.py[line:54] INFO train_pred: 0.9777  test_pred: 0.7247(修改create_dictionary后) 3DaysLabel
+    # lsi Wed, 16 Dec 2020 17:30:15 classifier.py[line:54] INFO train_pred: 0.9883  test_pred: 0.7123(修改create_dictionary后) 60DaysLabel
