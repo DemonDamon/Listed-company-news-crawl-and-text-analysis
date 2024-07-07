75c5fd395f058c30b62db215c785ab2754235838
dwliziran1 <bingzhenli@hotmail.com>
2024-07-07 11:46:33 +0800

    feat：创建文件解析组件

---
+++ src/components/docparser/__init__.py
@@ -0,0 +1,14 @@
+# Date    : 2024/6/23 23:48
+# File    : __init__.py
+# Desc    : 
+# Author  : Damon
+# E-mail  : bingzhenli@hotmail.com
+
+# from .pdf_parser import PdfParser as PdfParser, PlainParser
+# from .docx_parser import RAGFlowDocxParser as DocxParser
+# from .excel_parser import RAGFlowExcelParser as ExcelParser
+
+
+from .parser import Parser as DocParser
+from .ppt_parser import PPTParser as PptParser
+from .pdf_parser import PDFParser as PdfParser
